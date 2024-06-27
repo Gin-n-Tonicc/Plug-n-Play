fffe0cb87d7c85d1a7ef2419bd9bda08c054d6f5
BelisAliosmanova <107267662+BelisAliosmanova@users.noreply.github.com>
2024-06-27 15:51:30 +0300

    delete unused methods

--- server/src/main/java/com/example/plug_n_play/Crawler.java
+++ server/src/main/java/com/example/plug_n_play/Crawler.java
@@ -3,17 +3,8 @@ package com.example.plug_n_play;
 
 import edu.uci.ics.crawler4j.crawler.Page;
 import edu.uci.ics.crawler4j.crawler.WebCrawler;
-import edu.uci.ics.crawler4j.parser.BinaryParseData;
-import edu.uci.ics.crawler4j.parser.HtmlParseData;
-import edu.uci.ics.crawler4j.parser.TextParseData;
 import edu.uci.ics.crawler4j.url.WebURL;
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
 
-import java.io.*;
-import java.net.URI;
-import java.net.URISyntaxException;
-import java.util.HashMap;
 import java.util.regex.Pattern;
 
 
@@ -23,144 +14,13 @@ public class Crawler extends WebCrawler {
 
     private static final Pattern DOC_PATTERN = Pattern.compile(".*(\\.(doc|docx|txt|pdf))$");
 
-    public static String CRAWL_STORAGE = "c:/temp/siteground";
-    public static String CRAWL_URL = "https://siteground.com";
-    public static URI cralwUri = null;
-    public static boolean DYNAMIC_PAGES = false;
-    public static int dataSize = 0;
-    public static int maxDataMB = 50 * 1000000; // 50 MB
-
-    WebDriver webDriver = null;
-
-    public static HashMap<String, String> filesMap = new HashMap<String, String>();
-
-    /**
-     * Specify whether the given url should be crawled or not based on
-     * the crawling logic. Here URLs with extensions css, js, images etc will not be visited
-     */
     @Override
     public boolean shouldVisit(Page referringPage, WebURL url) {
-        System.out.println(url.getURL().toLowerCase());
-
-        try {
-            if (cralwUri == null)
-                cralwUri = new URI(CRAWL_URL);
-
-            URI uri = new URI(url.getURL());
-
-            String href = url.getURL().toLowerCase();
-
-            return !FILTERS.matcher(href).matches() &&
-
-                    // same domain or PDF/DOC on another domain
-                    (cralwUri.getHost().equals(uri.getHost()) || DOC_PATTERN.matcher(href).matches());
-        } catch (URISyntaxException e) {
-            // ErrorLogger.logException(e, url.getURL(), SITE_ID);
-            return false;
-        }
+        return false;
     }
 
-    /**
-     * This function is called when a page is fetched and ready
-     * to be processed by the program.
-     */
     @Override
     public void visit(Page page) {
-        String url = page.getWebURL().getURL();
-        System.out.println("URL: " + url);
 
-        String fileName = CRAWL_STORAGE + "/" + generateNameFromUrl(url);
-
-        System.out.println(url);
-        if (dataSize > maxDataMB) {
-            logger.info(">>> Exeeded max Data MB: " + dataSize);
-            this.myController.shutdown();
-        }
-
-        if (DOC_PATTERN.matcher(url).matches()) {
-            String extension = url.substring(url.lastIndexOf(".") + 1);
-            // .txt files
-            if (page.getParseData() instanceof TextParseData txtParseData) {
-                try {
-                    writeToFile(fileName, txtParseData.getTextContent(), url);
-                } catch (IOException e) {
-//	            	ErrorLogger.logException(e, fileName, SITE_ID);
-                }
-            }
-            // .doc and .pdf files
-            else if (page.getParseData() instanceof BinaryParseData) {
-                try {
-                    writeToFile(fileName, page.getContentData(), url);
-                } catch (IOException e) {
-//                	ErrorLogger.logException(e, fileName, SITE_ID);
-                }
-            }
-        }
-        //
-        // html files
-        //
-        else if (page.getParseData() instanceof HtmlParseData) {
-
-            if (!fileName.toLowerCase().endsWith(".html"))
-                fileName = fileName + ".html";
-
-            if (DYNAMIC_PAGES) {
-                try {
-                    webDriver.get(url);
-                    String content = webDriver.findElement(By.tagName("body")).getText();
-
-                    writeToFile(fileName, content, url);
-                } catch (IOException e) {
-//                	ErrorLogger.logException(e, url, SITE_ID);
-                }
-            } else {
-                HtmlParseData htmlParseData = (HtmlParseData) page.getParseData();
-                String text = htmlParseData.getText(); //extract text from page
-                String html = htmlParseData.getHtml(); //extract html from page
-
-                try {
-                    writeToFile(fileName, html, url);
-                } catch (IOException e) {
-//                	ErrorLogger.logException(e, fileName, SITE_ID);
-                }
-            }
-        }
     }
-
-
-    public static String generateNameFromUrl(String url) {
-
-        // Replace useless chareacters with UNDERSCORE
-        String uniqueName = url.replace("://", "_").replace(".", "_").replace("/", "_");
-        // Replace last UNDERSCORE with a DOT
-        uniqueName = uniqueName.substring(0, uniqueName.lastIndexOf('_'))
-                + "." + uniqueName.substring(uniqueName.lastIndexOf('_') + 1);
-        return uniqueName;
-    }
-
-    private void writeToFile(String fileName, String str, String url) throws IOException {
-        dataSize += str.length();
-
-        logger.info(fileName);
-
-        BufferedWriter writer = new BufferedWriter(new FileWriter(fileName));
-        writer.write(str);
-        writer.close();
-
-        filesMap.put(fileName, url);
-    }
-
-
-    private void writeToFile(String fileName, byte[] data, String url) throws IOException {
-        dataSize += data.length;
-
-        logger.info(fileName);
-
-        BufferedOutputStream writer = new BufferedOutputStream(new FileOutputStream(fileName));
-        writer.write(data);
-        writer.close();
-
-        filesMap.put(fileName, url);
-    }
-
 }
--- server/src/main/java/com/example/plug_n_play/CrawlerStart.java
+++ server/src/main/java/com/example/plug_n_play/CrawlerStart.java
@@ -1,5 +1,7 @@
 package com.example.plug_n_play;
 
+import com.example.plug_n_play.model.Site;
+import com.example.plug_n_play.repository.SiteRepository;
 import edu.uci.ics.crawler4j.crawler.CrawlConfig;
 import edu.uci.ics.crawler4j.crawler.CrawlController;
 import edu.uci.ics.crawler4j.fetcher.PageFetcher;
@@ -8,66 +10,35 @@ import edu.uci.ics.crawler4j.robotstxt.RobotstxtServer;
 
 public class CrawlerStart {
 
-    final static int MAX_CRAWL_DEPTH = 4;
-    final static int NUMBER_OF_CRAWELRS = 2;
-    final static boolean DYNAMIC_PAGES = false;
+    final static int MAX_CRAWL_DEPTH = -1;
+    final static int MAX_PAGES_COUNT = -1;
+    final static int NUMBER_OF_CRAWELRS = 10;
 
     static CrawlController controller = null;
 
-    /**
-     * Test run here
-     */
-    public static void main(String[] args) throws Exception {
-        int MAX_DOWNLOAD_SIZE = 1048576 * 10;
+    private final SiteRepository siteRepository;
 
-        String CRAWL_STORAGE = "c:/temp/myssp";
-        String CRAWL_URL = "https://m2mservices.com/";
-
-        run(CRAWL_URL, CRAWL_STORAGE, MAX_DOWNLOAD_SIZE, 100);
+    public CrawlerStart(SiteRepository siteRepository) {
+        this.siteRepository = siteRepository;
     }
 
-
-    public static void shutdown() {
-        controller.shutdown();
-    }
-
-    public static void run(String url, String storageFolder, int maxDataMB, int maxPagesCount) throws Exception {
-
-        /*
-         * Instantiate crawl config
-         */
+    public void run () throws Exception {
         CrawlConfig config = new CrawlConfig();
-        config.setCrawlStorageFolder(storageFolder);
         config.setMaxDepthOfCrawling(MAX_CRAWL_DEPTH);
         config.setIncludeBinaryContentInCrawling(true);
         config.setIncludeHttpsPages(true);
-        config.setMaxDownloadSize(maxDataMB);
-        config.setMaxPagesToFetch(maxPagesCount);
+        config.setMaxPagesToFetch(MAX_PAGES_COUNT);
         PageFetcher pageFetcher = new PageFetcher(config);
 
-        /*
-         * Instantiate controller for this crawl.
-         */
         RobotstxtConfig robotstxtConfig = new RobotstxtConfig();
         RobotstxtServer robotstxtServer = new RobotstxtServer(robotstxtConfig, pageFetcher);
         controller = new CrawlController(config, pageFetcher, robotstxtServer);
 
 
-        /*
-         * Add seed URLs
-         */
-        //controller.addSeed("https://siteground.com");
+        for (Site site : siteRepository.findAll()) {
+            controller.addSeed(site.getUrl());
+        }
 
-        Crawler.CRAWL_STORAGE = storageFolder;
-        Crawler.CRAWL_URL = url;
-        Crawler.DYNAMIC_PAGES = DYNAMIC_PAGES;
-        Crawler.maxDataMB = maxDataMB;
-        controller.addSeed(Crawler.CRAWL_URL);
-
-
-        /*
-         * Start the crawl.
-         */
         controller.start(Crawler.class, NUMBER_OF_CRAWELRS);
 
         controller.waitUntilFinish();
