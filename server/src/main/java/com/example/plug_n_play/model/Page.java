23f69f7c9a35b64be6a35878a3ad38e370868d13
BelisAliosmanova <107267662+BelisAliosmanova@users.noreply.github.com>
2024-06-27 15:33:25 +0300

    save NGOs in database

---
+++ server/src/main/java/com/example/plug_n_play/model/Page.java
@@ -0,0 +1,27 @@
+package com.example.plug_n_play.model;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Entity
+@Getter
+@Setter
+@ToString
+@RequiredArgsConstructor
+@Table(name = "pages")
+public class Page {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Long id;
+    @ManyToOne
+    @JoinColumn(name = "site_id")
+    private Site site;
+    private String title;
+    private String metaData;
+    private String data;
+    private String url;
+}
---
+++ server/src/main/java/com/example/plug_n_play/model/Site.java
@@ -0,0 +1,23 @@
+package com.example.plug_n_play.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Getter
+@Setter
+@ToString
+@RequiredArgsConstructor
+@Table(name = "sites")
+public class Site {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Long id;
+    private String title;
+    private String url;
+    private String type;
+    private String name;
+    private String email;
+    private String country;
+}
